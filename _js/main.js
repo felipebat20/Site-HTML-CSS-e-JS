561bcc971cb2df4858ea72a40d0a4ce81e3ef243
Felipe <felipebatista24680@gmail.com>
2020-01-08 08:50:31 -0300

    Aula 5 - Escola Front End

--- _js/main.js
+++ _js/main.js
@@ -1,1 +1,9 @@
-$('.slider-principal').slick();
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+
+});
